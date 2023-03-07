23c6c422a6d761e832bd7e32b2894d2ddc0a5bc0
LeidyMol <lady91molina@gmail.com>
2023-03-07 14:37:00 -0500

    version 2.0

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './modulos/dashboard/dashboard.component';
+import { LoginComponent } from './modulos/login/login.component';
+import { PrincipalComponent } from './modulos/principal.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 
+    path: '', component: PrincipalComponent,
+  children:[
+  { path: 'dashboard', component: DashboardComponent },
+  {path: '', redirectTo: '/dashboard', pathMatch: 'full'}
+            ]
+  },
+  {path: 'login', component: LoginComponent},
+
+];
+
+
+
+
+//const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/modulos/principal.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-principal',
+  templateUrl: './principal.component.html',
+  styleUrls: ['./principal.component.css']
+})
+export class PrincipalComponent {
+
+}
